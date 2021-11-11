faa25f00506a352355cbd95c0c1a50f7a48e4f93
NzSN <aydenlin@foxmail.com>
2021-11-11 11:41:18 +0800

    Add Args to Oper Inst.

--- include/DAL/oper_insts.hpp
+++ include/DAL/oper_insts.hpp
@@ -3,6 +3,7 @@
 #include <string>
 #include "DAL/instruction.hpp"
 #include "DAL/environment.hpp"
+#include <memory>
 
 
 using std::string;
@@ -10,10 +11,12 @@ using std::string;
 #ifndef OPER_INSTS_H
 #define OPER_INSTS_H
 
+using ArgsPtr = std::shared_ptr<std::vector<string>>;
+
 class Oper: public OPERInst {
 public:
-    Oper(string opcode_, string src_, string dst_) :
-        OPERInst(OPER_INST), opcode(opcode_), src(src_), dst(dst_) {}
+    Oper(string opcode_, string src_, string dst_, ArgsPtr args_) :
+        OPERInst(OPER_INST), opcode(opcode_), src(src_), dst(dst_), args(args_) {}
     Oper(PyObject *PyOper);
     ~Oper() {}
 
@@ -29,12 +32,17 @@ public:
         return dst;
     }
 
+    ArgsPtr getArgs() {
+        return args;
+    }
+
     void eval(DAL_Environment &env);
 
   private:
     string opcode;
     string src;
     string dst;
+    ArgsPtr args;
 };
 
 #endif /* OPER_INSTS_H */
